36bce168a2656efec13384e333c2198fe3bace5a
andreUmag <142335765+andreUmag@users.noreply.github.com>
2023-10-28 20:51:34 -0500

    Configuracion de rutas

--- routes/routes.py
+++ routes/routes.py
@@ -7,3 +7,28 @@ registers_routes = blueprints.Blueprint("register", __name__)
 def get_register():
     register=Registers.query.all()
     return jsonify(registers_schema.dump(register))
+
+@registers_routes.route('/post', methods=['POST'])
+def create_register():
+    try:
+        data = request.get_json()
+        db.session.add(Registers(**data))
+        db.session.commit()
+        return register_schema.jsonify(Registers(**data)),201
+    except Exception as e:
+        return jsonify({"error":"Error al crear el registro","details":str(e)}),400
+    
+@registers_routes.route('/delete/<id>', methods=['DELETE'])
+def delete_comment(id):
+    try:
+        register = Registers.query.get(id)
+        if not register:
+            return jsonify({"error":"Error al encontrar el registro"}), 404
+        
+        db.session.delete(register)
+        db.session.commit()
+        
+        return jsonify({"error":"Registro eliminado correctamente xd"}),200
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"error":"Error al eliminar el registro","details":str(e)}),500
